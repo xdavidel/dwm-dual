d4d61b2f8e14f625ae7a0f2ce30494b20930a80d
David Delarosa <xdavidel@gmail.com>
2020-04-16 16:46:58 +0300

    actualfullscreen patch

--- config.h
+++ config.h
@@ -102,7 +102,8 @@ static Key keys[] = {
 	/* { MODKEY,                       XK_Tab,    view,           {0} }, */
 	/* { MODKEY|SFTKEY,             XK_c,      killclient,     {0} }, */
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	/* { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_s,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4]} },
